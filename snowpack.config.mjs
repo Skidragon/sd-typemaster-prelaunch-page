b1b3f5e21848882e96e799114c34c3c360686122
Simon Davis <simonfreelance1@gmail.com>
2021-05-21 19:29:03 -0400

    configured snowpack and can use sass files now

---
+++ snowpack.config.mjs
@@ -0,0 +1,19 @@
+/** @type {import("snowpack").SnowpackUserConfig } */
+export default {
+  plugins: ["@snowpack/plugin-sass"],
+  https://www.snowpack.dev/guides/routing#scenario-1%3A-spa-fallback-paths
+  routes: [{ match: "routes", src: ".*", dest: "/starter-code/index.html" }],
+  optimize: {
+    /* Example: Bundle your final build: */
+    // "bundle": true,
+  },
+  packageOptions: {
+    /* ... */
+  },
+  devOptions: {
+    port: 3000,
+  },
+  buildOptions: {
+    /* ... */
+  },
+};
